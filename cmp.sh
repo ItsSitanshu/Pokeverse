080c1cf6d4bb84c06b4ae92d95fd89f191038ef6
ItsSitanshu <sitanshu15shrestha@gmail.com>
2022-09-06 19:31:55 +0545

    First commit
    Basic file structuring and cmake compiling/building

---
+++ cmp.sh
@@ -0,0 +1,15 @@
+# !/bin/sh
+
+if [ "$1" == "-c" ]; then
+    rm -rf build/CMakeFiles
+else
+    ls
+    cat LICENCE
+    cmake -B build
+    cd build
+    make
+fi
+
+if [ "$1" == "-d" ]; then
+    ./Pokeverse
+fi
